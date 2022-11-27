1694b4cda5e05b7230a3b58aee77ce2b74512d85
Stuart Nam <tokarinrika@gmail.com>
2022-11-27 16:12:57 +0700

    Update central server handler

--- central_server.py
+++ central_server.py
@@ -3,39 +3,69 @@ from threading import *
 
 # Database
 
-accounts = []
+accounts = [("tokarinrika", "liliana0"), ("harryhaha", "anhlaai")]
 active_threads = []
-active_conns = []
-active_user = [] #(username, IP address)
+done_threads = []
+active_accounts = [] #(IP address, username)
 # Server sending
-def server_feedback():
-    signal = True
-    pass
+def server_feedback(connection, msg):
+    conn, _ = connection
+    conn.send(msg.encode())
 
-def server_receive(conn):
+def server_request_handler(connection):
+    logged_in_flag = False
+    conn, addr = connection
     client_request = 1
     server_signal = 0
     
     while client_request != 0:
         client_request = int.from_bytes(conn.recv(1024))
         if client_request == 0:
+            if logged_in_flag:
             # Disconnect request
-            # Send disconnect message to send thread and disconnect
-            server_feedback(server_signal)
-            pass
+            # Send disconnect message to send thread and disconnect   
+                pass
+            else:
+                msg = "Disconnect from server"
+                server_feedback(connection, msg)
+                for active_thread in active_threads:
+                    _, conn = active_thread
+                    if conn == connection:
+                        done_threads.append(active_thread)
+                        active_threads.remove(active_thread)
+
         elif client_request == 1:
             server_feedback(server_signal)
             # Connect request
             pass
         elif client_request == 2:
             # Log in request
-            pass
+            username = conn.recv(1024).decode()
+            password = conn.recv(1024).decode()
+            account_info = (username, password)
+            if account_info in accounts:
+                active_accounts.append((addr, username))
+                msg = "Log in successfully"
+                logged_in_flag = True
+                server_feedback(conn, msg)
+            else:
+                msg = "Log in failed"
+                server_feedback(conn, msg)
         elif client_request == 3:
             # Register request
-            pass
+            username = conn.recv(1024).decode()
+            password = conn.recv(1024).decode()
+            account_info = (username, password)
+            accounts.append(account_info)
+            msg = "Register successfully"
+            server_feedback(conn, msg)
         elif client_request == 4:
             # Log out request
-            pass
+            for acc in active_accounts:
+                tmp, _ = acc
+                if tmp == addr:
+                    active_accounts.remove(acc)
+                    logged_in_flag = False
 
 # Handlers
 def mainloop():
@@ -47,12 +77,18 @@ def mainloop():
     cserver_socket.listen()
 
     while True:
-        conn, addr = cserver_socket.accept()
-        thread_send = Thread()
+        connection = cserver_socket.accept()
+
+        thread_server_request_handler = Thread(
+            target = server_request_handler,
+            args = (connection, )
+        )
+        thread_server_request_handler.start()
+
+        active_threads.append((thread_server_request_handler, connection))
+
+        for server_threads in done_threads:
+            server_threads.join()
 
 if __name__ == "__main__":
     mainloop()
-
-
-
-
